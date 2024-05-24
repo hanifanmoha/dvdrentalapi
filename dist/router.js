d68ce4a22c99669072742795e988f52f95c47e37
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-05-24 10:19:01 +0700

    feat: try mongoose mocked

--- dist/router.js
+++ dist/router.js
@@ -22,12 +22,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const controller = __importStar(require("./controller"));
+const mongodb_memory_server_1 = require("mongodb-memory-server");
+const mongoose_1 = __importDefault(require("mongoose"));
 const router = express_1.default.Router();
 const filmRouter = express_1.default.Router();
 filmRouter.get('/', controller.getFilms);
@@ -58,4 +69,46 @@ router.use('/categories', categoryRouter);
 router.use('/customers', customerRouter);
 router.use('/stores', storeRouter);
 router.use('/staff', staffRouter);
+router.get('/mocked', function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const mongod = yield mongodb_memory_server_1.MongoMemoryServer.create();
+        const conn = yield mongoose_1.default
+            .connect(mongod.getUri(), {})
+            .then(() => console.log('MongoDB connected'))
+            .catch((err) => console.log(err));
+        console.log(mongod.getUri());
+        const userSchema = new mongoose_1.default.Schema({
+            name: {
+                type: String,
+                required: true,
+            },
+            email: {
+                type: String,
+                required: true,
+                unique: true,
+            },
+            password: {
+                type: String,
+                required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+            },
+        });
+        const User = mongoose_1.default.model('User', userSchema);
+        const newUser = new User({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: 'password123',
+        });
+        yield newUser
+            .save()
+            .then(() => console.log('User created'))
+            .catch((err) => console.log(err));
+        const users = yield User.find();
+        console.log(users);
+        res.send(users);
+    });
+});
 exports.default = router;
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import express, { Router } from 'express';
 import * as controller from './controller';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
 
 const router: Router = express.Router();
 
@@ -39,5 +41,52 @@ router.use('/categories', categoryRouter);
 router.use('/customers', customerRouter);
 router.use('/stores', storeRouter);
 router.use('/staff', staffRouter);
+
+router.get('/mocked', async function (req, res) {
+  const mongod = await MongoMemoryServer.create();
+  const conn = await mongoose
+    .connect(mongod.getUri(), {})
+    .then(() => console.log('MongoDB connected'))
+    .catch((err: any) => console.log(err));
+  console.log(mongod.getUri());
+
+  const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+  const User = mongoose.model('User', userSchema);
+
+  const newUser = new User({
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password: 'password123',
+  });
+
+  await newUser
+    .save()
+    .then(() => console.log('User created'))
+    .catch((err: any) => console.log(err));
+
+  const users = await User.find();
+  console.log(users);
+
+  res.send(users);
+});
 
 export default router;
